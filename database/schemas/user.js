8aaceb095a4e06f5342f4350a3748a1b7ac0e5fa
Ayushmaan2001 <ayushmaanjangid2002@gmail.com>
2022-03-06 21:52:38 +0530

    user-db-1

---
+++ database/schemas/user.js
@@ -0,0 +1,22 @@
+export default {
+  title: "User",
+  name: "user",
+  type: "document",
+  fields: [
+    {
+      title: "First Name",
+      name: "first_name",
+      type: "string",
+    },
+    {
+      title: "Last Name",
+      name: "last_name",
+      type: "string",
+    },
+    {
+      title: "Bio",
+      name: "bio",
+      type: "text",
+    },
+  ],
+};
