6b1f895ba9f3cc1d872c518f9bb60d6b77d58ef3
Ayushmaan2001 <ayushmaanjangid2002@gmail.com>
2022-03-08 18:53:37 +0530

    form-frontend

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 First install all Node modules using node i
 
 # .env file
-Add tour own env file to the project
+Add your own env file to the project
 
 # Source
 https://www.youtube.com/watch?v=iRORiaLAgaE
--- insta-frontend/src/jsx/sign-up.jsx
+++ insta-frontend/src/jsx/sign-up.jsx
@@ -1,8 +1,35 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import "../css/signup.css";
+import { useRef, useState } from "react";
 
 function SignUp() {
-  return <></>;
+  const username = new useRef();
+  const first_name = new useRef();
+  const last_name = new useRef();
+  const [user_name, setUser_name] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+
+  const post = () => {
+    setUser_name(username.current.value);
+  };
+  const show = () => {
+    console.log(user_name);
+    console.log(firstname);
+    console.log(lastname);
+  };
+  return (
+    <>
+      <form id="form-control" className="align-item-center">
+        <label>Username</label>
+        <input type="text" placeholder="Username" className="mb-2" />
+        <label>First Name</label>
+        <input type="text" placeholder="Username" className="mb-2"/>
+        <label>Last Name</label>
+        <input type="text" placeholder="Username" className="mb-2"/>
+        <button className="btn btn-primary mt-1">Register</button>
+      </form>
+    </>
+  );
 }
 
 export default SignUp;
