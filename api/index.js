a2482a286bb13c0d2a6106178d72be01ddb22895
Ayushmaan2001 <ayushmaanjangid2002@gmail.com>
2022-03-08 19:25:26 +0530

    api-test

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 const app = require("express")();
 const bodyparser = require("body-parser");
+const core = require("cors");
+app.use(core());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
+
+const p = [];
+
+app.post("/register", (req, res) => {
+  p.push(req.body.user);
+  res.send("done");
+});
+
+app.get("/show", (req, res) => {
+  res.send(p);
+});
 
 app.listen(3001, () => {
   console.log("Started");
--- insta-frontend/src/jsx/sign-up.jsx
+++ insta-frontend/src/jsx/sign-up.jsx
@@ -1,32 +1,60 @@
 import "../css/signup.css";
 import { useRef, useState } from "react";
+import axios from "axios";
 
 function SignUp() {
   const username = new useRef();
   const first_name = new useRef();
   const last_name = new useRef();
-  const [user_name, setUser_name] = useState("");
-  const [firstname, setFirstname] = useState("");
-  const [lastname, setLastname] = useState("");
+  // const [user_name, setUser_name] = useState("");
+  // const [firstname, setFirstname] = useState("");
+  // const [lastname, setLastname] = useState("");
 
-  const post = () => {
-    setUser_name(username.current.value);
+  function done(e){
+    e.preventDefault();
+  }
+  const register = () => {
+    axios
+      .post("http://localhost:3001/register", {
+        user: username.current.value,
+        first: first_name.current.value,
+        last: last_name.current.value,
+      })
+      .then((res) => {
+        alert(res.data);
+      });
   };
-  const show = () => {
-    console.log(user_name);
-    console.log(firstname);
-    console.log(lastname);
-  };
+
+  // const show = () => {
+  //   console.log(user_name);
+  //   console.log(firstname);
+  //   console.log(lastname);
+  // };
   return (
     <>
-      <form id="form-control" className="align-item-center">
+      <form id="form" className="align-item-center" onSubmit={(event) => {done(event)}}>
         <label>Username</label>
-        <input type="text" placeholder="Username" className="mb-2" />
+        <input
+          type="text"
+          placeholder="Username"
+          className="mb-2"
+          ref={username}
+        />
         <label>First Name</label>
-        <input type="text" placeholder="Username" className="mb-2"/>
+        <input
+          type="text"
+          placeholder="Username"
+          className="mb-2"
+          ref={first_name}
+        />
         <label>Last Name</label>
-        <input type="text" placeholder="Username" className="mb-2"/>
-        <button className="btn btn-primary mt-1">Register</button>
+        <input
+          type="text"
+          placeholder="Username"
+          className="mb-2"
+          ref={last_name}
+        />
+        <button className="btn btn-primary mt-1" onClick={register}>Register</button>
       </form>
     </>
   );
